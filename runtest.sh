12e9e401e2396e61ce2302817f3823b57673aa76
deeppadmani <125155392+deeppadmani@users.noreply.github.com>
2024-08-26 12:24:41 -0500

    setup test

---
+++ runtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run iscas with different parameters
+./iscas c17.bench G1gat G22gat
+./iscas c17.bench G1gat G2gat
+./iscas c17.bench G22gat G23gat
+./iscas c17.bench G1gat G10gat
+./iscas c880.bench G8gat G866gat
+./iscas c7552.bench G9 G416
